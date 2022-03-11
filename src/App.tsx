86e0152aed637f1de62c783fa138fa95d273a86f
JosephatR <josephat@elevar.mx>
2022-03-10 21:44:09 -0600

    last updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Game from './Pages/Game';
 function App() {
-  
+
   return (
 
     <div className="h-screen w-100 overflow-y-auto bg-white dark:bg-dark-bg overflow-x-hidden">
-      <Routes >
-        <Route path="*" element={<Game />} />
-      </Routes>
+      <Game />
     </div>
   );
 }
